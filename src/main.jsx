5676c321f66f53e28201e480e371e2fa12584b0d
dollyazur <azurgrisby@hotmail.fr>
2025-01-29 14:49:02 +0100

    creation fichier redux et store.js, changement ok dans main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // Import de Redux
+import { store } from './redux/store'; // Import du store
 import App from './App'; // Ton composant principal
 
 import './assets/css/main.css'; // Ton fichier CSS principal
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}> {/* Fournit le store à toute l'application */}
+            <App />
+        </Provider>
     </React.StrictMode>
 );
---
+++ src/redux/store.js
@@ -0,0 +1,7 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+    reducer: {}, // On ajoutera les reducers ici plus tard
+});
+
+export default store;
