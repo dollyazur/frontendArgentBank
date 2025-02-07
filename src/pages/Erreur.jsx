8efedcdb6c1ccb30049c10360ae5f0587d887d98
dollyazur <azurgrisby@hotmail.fr>
2025-02-07 09:46:19 +0100

    correction page erreur

--- src/pages/Erreur.jsx
+++ src/pages/Erreur.jsx
@@ -1,13 +1,18 @@
 import image from "../assets/images/android-chrome-512x512.png";
 import "../assets/css/main.css";
+import Footer from "../components/footer/footer";
 
 function Error_404() {
   return (
-    <div className="error_404">
+    <>
+    <main className="error_404">
       <img src={image} alt="404 en vert" />
-      <h2> Erreur! La page que vous demandez n'existe pas.</h2>
+      <h1> Erreur! La page que vous demandez n'existe pas.</h1>
       <a href="/">Retourner sur la page d'accueil</a>
-    </div>
+    </main>
+
+<Footer />
+</>
   );
 }
 
